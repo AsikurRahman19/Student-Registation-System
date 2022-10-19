77d5d67724d028dfd6ee7a51fe75d51db3fc52b4
AsikurRahman19 <asikur15-2373@diu.edu.bd>
2022-10-19 11:29:08 +0600

    Initial commit

---
+++ FrmLogin.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Student_Registation
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TbUsername = new System.Windows.Forms.TextBox();
+            this.TbPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(175, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // TbUsername
+            // 
+            this.TbUsername.Location = new System.Drawing.Point(315, 101);
+            this.TbUsername.Multiline = true;
+            this.TbUsername.Name = "TbUsername";
+            this.TbUsername.Size = new System.Drawing.Size(255, 47);
+            this.TbUsername.TabIndex = 2;
+            // 
+            // TbPassword
+            // 
+            this.TbPassword.Location = new System.Drawing.Point(316, 175);
+            this.TbPassword.Multiline = true;
+            this.TbPassword.Name = "TbPassword";
+            this.TbPassword.Size = new System.Drawing.Size(255, 47);
+            this.TbPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(316, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 568);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TbPassword);
+            this.Controls.Add(this.TbUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TbUsername;
+        private System.Windows.Forms.TextBox TbPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ FrmLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Registation
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            string ConStr = (@"Data Source =DESKTOP-DQA0BC1\SQLEXPRESS;Initial Catalog=SignUp;Integrated Security=true");
+            SqlConnection con;
+            SqlDataReader dr;
+            public FrmLogin()
+            {
+                InitializeComponent();
+                con = new SqlConnection(ConStr);
+            }
+
+            private void BtnLogin_Click(object sender, EventArgs e)
+            {
+                string query = @"SELECT 
+      [Username]
+      ,[Password]
+  FROM [dbo].[Tbl_SignUp] Where Username='" + TbUsername.Text + "'AND Password='" + TbPassword.Text + "'";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    Form1 frm = new Form1();
+                    this.Hide();
+                    frm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or Password");
+                }
+
+            }
+        
+
